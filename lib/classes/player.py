523a0d70d6e8b56550f2d558b83b6c302ce00156
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-30 18:15:01 -0700

    Complete mock CC

--- lib/classes/player.py
+++ lib/classes/player.py
@@ -6,22 +6,49 @@ class Player:
         self.username = username
         self._results = []
         self._games_played = []
+        type(self).all.append(self)
         
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        if new_result and isinstance(new_result, Result): 
+            self._results.append(new_result)
+        return self._results
     
     def games_played(self, new_game=None):
         from classes.game import Game
-        pass
+        if new_game and isinstance(new_game, Game): 
+            self._games_played.append(new_game)
+        return self._games_played 
     
     def played_game(self, game):
-        pass
+        return game in self._games_played
     
     def num_times_played(self, game):
-        pass
+        results = [r for r in self._results if r.game == game]
+        return len(results)
     
     @classmethod
     def highest_scored(cls, game):
-        pass
+        high_score = 0
+        best_player = None
+        for player in Player.all: 
+            avg = game.average_score(player)
+            if avg and avg > high_score: 
+                high_score = avg 
+                best_player = player 
+        return best_player 
+
+        avg_scores = [game.average_score(player) for player in Player.all] 
+        return max(avg_scores)
         
+    # Properties 
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if type(username) == str and (2 <= len(username) <= 16):
+            self._username = username
+        else: 
+            raise TypeError("Username must be a string of 2-16 characters")
--- lib/classes/result.py
+++ lib/classes/result.py
@@ -6,3 +6,48 @@ class Result:
         self.player = player
         self.game = game
         self.score = score
+
+        type(self).all.append(self)
+
+        player.results(self)
+        player.games_played(game)
+
+        game.results(self)
+        game.players(player)
+
+    # Properties 
+    @property
+    def score(self): 
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if type(score) == int and (1 <= score <= 5000):
+            self._score = score
+        else: 
+            raise TypeError("Score must be an integer between 1 and 5000")
+    
+    @property
+    def player(self):
+        return self._player
+    
+    @player.setter
+    def player(self, player):
+        from .player import Player
+        if isinstance(player, Player):
+            self._player = player 
+        else: 
+            raise Exception 
+    
+    @property
+    def game(self):
+        return self._game
+    
+    @game.setter
+    def game(self, game):
+        from .game import Game 
+        if isinstance(game, Game):
+            self._game = game 
+        else: 
+            raise Exception 
+    
